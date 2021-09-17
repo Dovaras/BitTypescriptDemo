a0c98e430bf03eb916f5d1346024fa825a946e8e
Arūnas Dovainis <arunas.dovainis@gmail.com>
2021-09-17 11:06:59 +0300

    Wednesday lecture

--- js/script.js
+++ js/script.js
@@ -9,15 +9,42 @@ button.addEventListener("click", function () {
     resultArea.innerText = "| Result: ";
     resultArea.innerText += add(+input1.value, +input2.value);
 });
-// class Person(name0: string) {
-//     this.name0;
-//     constructor(name0) {
-//         this.name0 = name0;
-//     }
-// }
+var Person = /** @class */ (function () {
+    function Person(name, surname, age, grades) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.grades = grades;
+    }
+    Person.prototype.getTotalNameLength = function () {
+        return this.name.length + this.surname.length;
+    };
+    Person.prototype.getGradesAverage = function () {
+        var sumOfGrades = 0;
+        for (var i = 0; i < this.grades.length; i++) {
+            sumOfGrades += this.grades[i];
+        }
+        return sumOfGrades / this.grades.length;
+    };
+    return Person;
+}());
+var people = [
+    new Person("Mindaugas", "Dovainis", 152, [10, 5, 8]),
+    new Person("Jonas", "Petrikas", 123, [8, 7, 9])
+];
+// ... alternatyvi duomenu reprezentacija kiumpiuteryje:
+var mindaugas = { name: "Mindaugas", surname: "Kaubojus", age: 51, grades: [5, 10, 8, 10, 3] };
+// resultArea.innerText += "\n" + people[0].name + " " + people[0].surname + " " + people[0].age;
+// resultArea.innerText += "\n" + people[1].name + " " + people[1].surname + " " + people[1].age;
+var totalAge = 0;
+for (var i = 0; i < people.length; i++) {
+    totalAge += people[i].age;
+}
+console.log("These people have combined " + totalAge + " years of life experience, the average age " + totalAge / people.length);
+people.forEach(function (person) { return console.log(person.getGradesAverage()); });
 var someVariable;
 someVariable = 13;
 console.log(someVariable.toString());
 var anyType;
-anyType = true;
+anyType = false;
 console.log(anyType.valueOf());
--- script.ts
+++ script.ts
@@ -12,17 +12,52 @@ button.addEventListener("click", () => {
     resultArea.innerText += add(+input1.value, +input2.value);
 });
 
-// class Person(name0: string) {
-//     this.name0;
-//     constructor(name0) {
-//         this.name0 = name0;
-//     }
-// }
+class Person {
+    name: string;
+    surname: string;
+    age: number;
+    grades: number[];
+    constructor(name: string, surname: string, age: number, grades: number[]) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.grades = grades;
+    }
+    getTotalNameLength(): number {
+        return this.name.length + this.surname.length;
+    }
+    getGradesAverage(): number {
+        let sumOfGrades = 0;
+        for (let i = 0; i < this.grades.length; i++) {
+            sumOfGrades += this.grades[i];
+        }
+        return sumOfGrades / this.grades.length;        
+    }
+}
 
-let someVariable: boolean | number;
-someVariable = 13;
-console.log(someVariable.toString())
+let people: Person[] = [
+    new Person("Mindaugas", "Dovainis", 152, [10, 5, 8]),
+    new Person("Jonas", "Petrikas", 123, [8, 7, 9])
+]
 
-let anyType: any;
-anyType = false;
-console.log(anyType.valueOf())
+// ... alternatyvi duomenu reprezentacija kiumpiuteryje:
+let mindaugas = { name: "Mindaugas", surname: "Kaubojus", age: 51, grades: [5, 10, 8, 10, 3] }
+// resultArea.innerText += "\n" + people[0].name + " " + people[0].surname + " " + people[0].age;
+// resultArea.innerText += "\n" + people[1].name + " " + people[1].surname + " " + people[1].age;
+
+let totalAge = 0;
+for (let i = 0; i < people.length; i++) {
+    totalAge += people[i].age;
+}
+
+console.log(`These people have combined ${totalAge} years of life experience, the average age ${totalAge / people.length}`);
+
+people.forEach(person => console.log(person.getGradesAverage()))
+
+// let someVariable: boolean | number;
+// someVariable = 13;
+// console.log(someVariable.toString())
+
+// let anyType: any;
+// anyType = false;
+// console.log(anyType.valueOf())
